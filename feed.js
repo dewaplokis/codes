2ed848efe597d30e3a9823f8b45f0cc0b6d33c09
dewaplokis <dewaplokis@gmail.com>
2020-10-01 14:52:15 +0800

    Delete feed.js

--- feed.js
+++
@@ -1,3 +0,0 @@
- <b:loop values='data:post.labels' var='label'>
-            <script expr:src='&quot;/feeds/posts/summary/-/&quot; + data:label.name + &quot;?alt=json-in-script&amp;callback=bacaJuga&amp;max-results=5&quot;'/>
-        </b:loop>
